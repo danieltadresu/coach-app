a010ecbd5570cfb5933ef45470d473f6d60802f8
Dani <daniaxasabraxas@gmail.com>
2020-11-10 11:23:25 -0300

    Working on the main layout and and styling

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,6 @@
   </router-view>
 </template>
 
-<script>
-
-</script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -14,6 +10,18 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
