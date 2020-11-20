fcc26497ea388b41bff9040a5c57260286ff3c92
Dani <daniaxasabraxas@gmail.com>
2020-11-20 13:44:54 -0300

    Working on base layouts

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -8,7 +8,19 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul>
-      LIST OF COACHES
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
     </ul>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    }
+  }
+}
+</script>
