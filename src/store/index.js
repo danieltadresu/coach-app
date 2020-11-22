51ba848ec8ae6b1f9c55cbfec572cea1e5247129
Dani <daniaxasabraxas@gmail.com>
2020-11-22 13:39:31 -0300

    Outputting incoming requests (contact messages to coaches)

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { createStore } from 'vuex';
 import coachesModule from './modules/coaches/index.js';
-import requestModule from './modules/requests/index.js';
+import requestsModule from './modules/requests/index.js';
 
 const store = createStore({
   modules: {
     coaches: coachesModule,
-    requests: requestModule
+    requests: requestsModule
   },
   state() {
     return {
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -6,6 +6,6 @@ export default {
       userEmail: payload.email,
       message: payload.message
     };
-    context.commit('addRequest', newRequest);
+    context.commit('addRequest', newRequest); 
   }
-};
+}
